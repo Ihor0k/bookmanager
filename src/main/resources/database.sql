4b351e4c728c13612f4eda3c859965d50544490e
Ihor0k <b.ihor@yandex.ru>
2016-11-29 00:00:15 +0200

    Created 'comments' table

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -4,7 +4,7 @@ CREATE TABLE books (
   title       VARCHAR(255) NOT NULL,
   author      VARCHAR(255) NOT NULL,
   description VARCHAR(2047)         DEFAULT NULL,
-  filename    VARCHAR(255)          DEFAULT NULL,
+  filename    VARCHAR(255) NOT NULL,
   user_id     INT          NOT NULL,
 
   FOREIGN KEY (user_id) REFERENCES users (id)
@@ -17,5 +17,18 @@ CREATE TABLE users (
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   role     VARCHAR(255) NOT NULL
+)
+  ENGINE = InnoDB;
+
+#Table: comment
+CREATE TABLE comments (
+  id      INT           NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  text    VARCHAR(2047) NOT NULL,
+  time    DATETIME      NOT NULL,
+  user_id INT           NOT NULL,
+  book_id INT           NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (book_id) REFERENCES books (id)
 )
   ENGINE = InnoDB;
